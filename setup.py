b2dc58d6949de7467da87c6fa07bfb60c9988445
Sandeep Prasad <iamprasadsandeep@gmaiil.com>
2025-05-11 03:18:58 +0530

    removed unnecessary file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name="clickindex",
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires = [
     'pandas==2.2.3',
